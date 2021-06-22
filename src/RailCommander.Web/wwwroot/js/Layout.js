46cb7837dcd86898fa9c4be156365e1eb9b087bb
James Thorpe <james@pawsforthorpe.co.uk>
2021-06-22 17:31:17 +0100

    fiddling

--- src/RailCommander.Web/wwwroot/js/Layout.js
+++ src/RailCommander.Web/wwwroot/js/Layout.js
@@ -15,33 +15,35 @@ class trackBlock {
 
 const layout = Vue.reactive({
     TrackBlocks: [],
-    Signals: []
+    Signals: [],
+    createTrackBlock: function(state) {
+        const tb = Vue.reactive(new trackBlock(state));
+        this.TrackBlocks.push(tb);
+        return tb;
+    }
 });
 
-var tb1 = new trackBlock("reserved");
-layout.TrackBlocks.push(tb1);
+var tb1 = layout.createTrackBlock("unreserved");
 tb1.addTrackSection(new trackStraight(1, 1, 0));
 tb1.addTrackSection(new trackStraight(2, 1, 0));
 tb1.addTrackSection(new trackStraight(3, 1, 0));
 tb1.addTrackSection(new trackTurnoutLeft(4, 1, 0, "normal"));
 
-var tb2 = new trackBlock("unreserved");
+var tb2 = layout.createTrackBlock("unreserved");
 tb2.addTrackSection(new trackCurveLeft(5, 0, 180));
-var tb3 = new trackBlock("occupied");
+
+var tb3 = layout.createTrackBlock("occupied");
 tb3.addTrackSection(new trackStraight(5, 1, 0));
-layout.TrackBlocks.push(tb2);
-layout.TrackBlocks.push(tb3);
 
 layout.Signals.push(new trackSignal(5, 1, 0, "danger"));
 layout.Signals.push(new trackSignal(1, 1, 180, "caution"));
 
 
 window.setInterval(function () {
-    var b = layout.TrackBlocks[0];
-    if (b.trackSections[1].state === "occupied") {
-        b.trackSections[1].state = "reserved";
+    if (tb1.state === "occupied") {
+        tb1.state = "reserved";
     } else {
-        b.trackSections[1].state = "occupied";
+        tb1.state = "occupied";
     }
 },
 1000);
--- src/RailCommander.Web/wwwroot/vues/TrackPlan/CurveLeft.vue
+++ src/RailCommander.Web/wwwroot/vues/TrackPlan/CurveLeft.vue
@@ -1,3 +1,3 @@
 ﻿<template>
-    <path d="M 0, 16 Q 16 16, 32 0" fill="transparent" />asfas
+    <path d="M 0, 16 Q 16 16, 32.1 -0.1" fill="transparent" />asfas
 </template>
